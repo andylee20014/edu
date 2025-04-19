e762af81226bc3fdb9fb7c6e93871f9937523c8d
Dalong <andylee2014@gmail.com>
2025-04-19 16:09:35 +0800

    Update server.js

--- server.js
+++ server.js
@@ -898,6 +898,6 @@ app.post('/api/admin/batch-prefixes', async (req, res) => {
 });
 
 // 启动服务器
-app.listen(PORT, () => {
-  console.log(`服务器运行在 http://localhost:${PORT}`);
-}); 
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`服务器运行在 http://0.0.0.0:${PORT}`);
+});
